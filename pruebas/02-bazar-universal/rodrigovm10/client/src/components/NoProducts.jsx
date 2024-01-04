3d79b358a403e1803e3ecb7514f525317331a5e7
rodrigovm10 <rv0611200369@gmail.com>
2024-01-04 12:53:39 -0600

    proyecto nuevo

--- pruebas/02-bazar-universal/rodrigovm10/client/src/components/NoProducts.jsx
+++
@@ -1,5 +0,0 @@
-export function NoProducts() {
-	return (
-		<h1 className='text-center text-2xl font-semibold mt-20'>No hay productos para su búsqueda.</h1>
-	)
-}
--- pruebas/02-bazar-universal/rodrigovm10/client/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { Home } from './Home'
-export { Products } from './Products'
-export { Product } from './Product'
