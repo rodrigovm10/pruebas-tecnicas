4412bc2babf4cfc874934e9238f8bae2da2c6a68
rodrigovm10 <rv0611200369@gmail.com>
2024-01-08 16:11:34 -0600

    correccion direccion fetch

--- pruebas/02-bazar-universal/rodrigovm10/src/client/libs/data.ts
+++ pruebas/02-bazar-universal/rodrigovm10/src/client/libs/data.ts
@@ -1,11 +1,11 @@
 export const fetchProducts = async (product: string) => {
-  const res = await fetch(`http://localhost:3000/api/items?q=${product}`)
+  const res = await fetch(`https://pruebas-tecnicas-bazar-online.vercel.app/api/items?q=${product}`)
   const data = await res.json()
   return data
 }
 
 export const fetchProduct = async (id: string) => {
-  const res = await fetch(`http://localhost:3000/api/items/${id}`)
+  const res = await fetch(`https://pruebas-tecnicas-bazar-online.vercel.app/api/items/${id}`)
   const data = await res.json()
   return data
 }
