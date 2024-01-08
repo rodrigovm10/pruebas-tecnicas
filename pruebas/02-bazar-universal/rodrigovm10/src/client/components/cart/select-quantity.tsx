8ff3fad476ed7c8fa75f1abd41e54d93dd94f4b3
rodrigovm10 <rv0611200369@gmail.com>
2024-01-07 22:42:57 -0600

    nada

--- pruebas/02-bazar-universal/rodrigovm10/src/client/components/cart/select-quantity.tsx
+++ pruebas/02-bazar-universal/rodrigovm10/src/client/components/cart/select-quantity.tsx
@@ -1,38 +1,38 @@
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from '@/client/components/ui/select'
-import { useCartStore } from '@/client/store/cartStore'
-import { type Product } from '@/types/types'
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue
+// } from '@/client/components/ui/select'
+// import { useCartStore } from '@/client/store/cartStore'
+// import { type Product } from '@/types/types'
 
-export function SelectQuantity({ quantity, product }: { quantity: number; product: Product }) {
-  const changeQuantity = useCartStore(state => state.changeQuantity)
+// export function SelectQuantity({ quantity, product }: { quantity: number; product: Product }) {
+//   const changeQuantity = useCartStore(state => state.changeQuantity)
 
-  return (
-    <Select
-      onValueChange={value => {
-        changeQuantity(product, Number(value))
-      }}
-      defaultValue={quantity.toString()}
-    >
-      <SelectTrigger className='w-[200px]'>
-        <SelectValue placeholder={`Cantidad: ${quantity}`} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Cantidad: </SelectLabel>
-          <SelectItem value='1'>Cantidad: 1</SelectItem>
-          <SelectItem value='2'>Cantidad: 2</SelectItem>
-          <SelectItem value='3'>Cantidad: 3</SelectItem>
-          <SelectItem value='4'>Cantidad: 4</SelectItem>
-          <SelectItem value='5'>Cantidad: 5</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
-}
+//   return (
+//     <Select
+//       onValueChange={value => {
+//         changeQuantity(product, Number(value))
+//       }}
+//       defaultValue={quantity.toString()}
+//     >
+//       <SelectTrigger className='w-[200px]'>
+//         <SelectValue placeholder={`Cantidad: ${quantity}`} />
+//       </SelectTrigger>
+//       <SelectContent>
+//         <SelectGroup>
+//           <SelectLabel>Cantidad: </SelectLabel>
+//           <SelectItem value='1'>Cantidad: 1</SelectItem>
+//           <SelectItem value='2'>Cantidad: 2</SelectItem>
+//           <SelectItem value='3'>Cantidad: 3</SelectItem>
+//           <SelectItem value='4'>Cantidad: 4</SelectItem>
+//           <SelectItem value='5'>Cantidad: 5</SelectItem>
+//         </SelectGroup>
+//       </SelectContent>
+//     </Select>
+//   )
+// }
